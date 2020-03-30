f26b5e26d2b4067096511648f71338d9f57c300b
minwoo.lee <minwoo.lee@northstar.co.kr>
2020-03-30 14:13:46 +0900

    test

---
+++ test/bbb.py
@@ -0,0 +1,30 @@
+﻿from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': True,
+    'start_date': datetime(2020,3,29),
+    'wait_for_downstream' : True,
+    'schedule_interval': '@once'
+    }
+
+dag = DAG(
+    'kubernetes_sample', default_args=default_args , catchup = True,)
+
+
+start = DummyOperator(task_id='run_this_first', dag=dag)
+
+run_this = BashOperator(
+    task_id='run_after_loop',
+    bash_command='echo 1',
+    dag=dag,
+)
+
+start >> run_this
+
+if __name__ == "__main__":
+    dag.cli()
