bddbddfd8aeba33cd9cd2eac792fc6b77e37863c
minwoo.lee <minwoo.lee@northstar.co.kr>
2020-03-30 14:54:11 +0900

    test

--- test/bbb.py
+++ test/bbb.py
@@ -37,37 +37,37 @@ dag = DAG(
     dagrun_timeout=timedelta(minutes=60),
 )
 
-run_this_last1 = DummyOperator(
-    task_id='run_this_last1',
+run_this_last = DummyOperator(
+    task_id='run_this_last',
     dag=dag,
 )
 
 # [START howto_operator_bash]
-run_this1 = BashOperator(
-    task_id='run_after_loop1',
+run_this = BashOperator(
+    task_id='run_after_loop',
     bash_command='echo 1',
     dag=dag,
 )
 # [END howto_operator_bash]
 
-run_this1 >> run_this_last1
+run_this >> run_this_last
 
 for i in range(3):
-    task1 = BashOperator(
-        task_id='runme_' + str(i)+str(1),
+    task = BashOperator(
+        task_id='runme_' + str(i),
         bash_command='echo "{{ task_instance_key_str }}" && sleep 1',
         dag=dag,
     )
-    task1 >> run_this1
+    task >> run_this
 
 # [START howto_operator_bash_template]
-also_run_this1 = BashOperator(
-    task_id='also_run_this1',
+also_run_this = BashOperator(
+    task_id='also_run_this',
     bash_command='echo "run_id={{ run_id }} | dag_run={{ dag_run }}"',
     dag=dag,
 )
 # [END howto_operator_bash_template]
-also_run_this1 >> run_this_last1
+also_run_this >> run_this_last
 
 if __name__ == "__main__":
     dag.cli()
