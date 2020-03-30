774b70453aeb87d7b70410c277c5526de3856b89
minwoo.lee <minwoo.lee@northstar.co.kr>
2020-03-30 14:29:12 +0900

    test

--- test/bbb.py
+++ test/bbb.py
@@ -2,11 +2,12 @@
 from datetime import datetime, timedelta
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils.dates import days_ago
 
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2020,3,29),
+    'start_date': days_ago(2),
     }
 
 dag = DAG(
@@ -17,17 +18,17 @@ dag = DAG(
     )
 
 
-start = DummyOperator(task_id='run_this_first',
-                      dag=dag,
-                      )
+run_this_last = DummyOperator(
+    task_id='run_this_last',
+    dag=dag,
+)
 
 run_this = BashOperator(
     task_id='run_after_loop',
     bash_command='echo 1',
     dag=dag,
 )
-
-start >> run_this
+run_this >> run_this_last
 
 if __name__ == "__main__":
     dag.cli()
