e77e69263eede96e9a0cd814990a7ed41b084ac0
minwoo.lee <minwoo.lee@northstar.co.kr>
2020-03-30 10:56:01 +0900

    test

--- test/tt.py
+++ test/tt.py
@@ -1,4 +1,7 @@
-﻿from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+﻿import airflow
+from airflow import DAG
+
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.contrib.kubernetes.secret import Secret
 from airflow.contrib.kubernetes.volume import Volume
 from airflow.contrib.kubernetes.volume_mount import VolumeMount
