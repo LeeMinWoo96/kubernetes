842c3848e69e27b8523910014d6f97e7384c327d
root <LeeMinWoo96@github.com>
2020-06-22 17:39:59 +0900

    externel_postgre

---
+++ externel_postgre_airflow_okd4/scc.sh
@@ -0,0 +1,7 @@
+oc adm policy add-scc-to-user anyuid system:serviceaccount:airflow:airflow-cluster-access
+
+oc adm policy add-scc-to-user anyuid system:serviceaccount:airflow:default
+
+oc adm policy add-scc-to-user hostaccess system:serviceaccount:airflow:airflow-cluster-access
+
+
